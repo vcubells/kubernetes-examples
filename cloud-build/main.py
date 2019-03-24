ec28aea0615b68d8c1eda16e0019f08cdd61d237
Vicente Cubells <vcubells@gmail.com>
2019-03-23 22:45:09 -0600

    Se modificó el endpoint

--- cloud-build/main.py
+++ cloud-build/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'Soy una API con Cloud Build'
+    return 'Soy una API con Cloud Build v2'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
