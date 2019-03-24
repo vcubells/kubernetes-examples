9fd9eda485b7a599d3f40a04211f594c4f0dcde0
Vicente Cubells <vcubells@gmail.com>
2019-03-23 22:42:07 -0600

    Se adicionó cloudbuild.yaml

--- cloud-build/main.py
+++ cloud-build/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'Soy una API'
+    return 'Soy una API con Cloud Build'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
